a78d8f34f325dc3b577cf44140e52154faabb5d5
Llanyro <Llanyro@gmail.com>
2022-07-07 17:29:29 +0200

    Adding headers from llcpp core

---
+++ asciidefines.hpp
@@ -0,0 +1,38 @@
+/*
+ * asciidefines.hpp
+ *
+ *  Created on: Nov 16, 2020
+ *      Author: llanyro
+ */
+
+#ifndef CORE_ASCIIDEFINES_HPP_
+#define CORE_ASCIIDEFINES_HPP_
+
+#define MAYUS 65
+#define MINUS 97
+#define NUMS 48
+#define DIFERENCIA_MAYUS_MINUS 32
+#define ASCII_CHARS 256
+#define ABECEDARIO_1 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+#define ABECEDARIO_1_LEN 52
+#define ABECEDARIO_2 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define ABECEDARIO_2_LEN 26
+#define ABECEDARIO_3 "abcdefghijklmnopqrstuvwxyz"
+#define ABECEDARIO_3_LEN 26
+#define ABECEDARIO_NUMS_1 "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+#define ABECEDARIO_NUMS_1_LEN 62
+#define ABECEDARIO_NUMS_2 "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define ABECEDARIO_NUMS_2_LEN 36
+#define ABECEDARIO_NUMS_3 "0123456789abcdefghijklmnopqrstuvwxyz"
+#define ABECEDARIO_NUMS_3_LEN 36
+#define CHARS_NUMS "0123456789"
+#define CHARS_NUMS_LEN 10
+
+#define IP_CHARS "0123456789."
+#define IP_CHARS_NUMS_LEN 11
+
+
+#define VISIBLE_CHARS "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\\|@#~½¬{[]}~<>.,;:-_´Ç`+*^¨ç¡'"
+#define VISIBLE_CHARS_LEN 93
+
+#endif /* CORE_ASCIIDEFINES_HPP_ */
---
+++ llanythrow.hpp
@@ -0,0 +1,16 @@
+/*
+ * llanythrow.hpp
+ *
+ *  Created on: ??? ??, ????
+ *      Author: llanyro
+ */
+
+#ifndef CORE_LLANYTHROW_HPP_
+#define CORE_LLANYTHROW_HPP_
+
+#include "llanytypeslib.hpp"
+
+// If false, then exit program
+#define ll_assert(a) (a ? TRUE : exit(1))
+
+#endif /* CORE_LLANYTHROW_HPP_ */

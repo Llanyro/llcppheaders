e0beab20f6f67043d867fadf2e2ed1108e44edea
Llanyro <Llanyro@gmail.com>
2024-03-25 20:53:03 +0100

    Add singleton base

---
+++ llanylib/Singleton.hpp
@@ -0,0 +1,82 @@
+/*
+ *	Singleton.hpp
+ *
+ *	Author: Francisco Julio Ruiz Fernandez
+ *	Author: llanyro
+ *
+ *	Version: 4.2
+ */
+
+#if defined(LLANYLIB_SINGLETON_HPP_) // Guard && version protector
+	#if LLANYLIB_SINGLETON_MAYOR_ != 4 || LLANYLIB_SINGLETON_MINOR_ < 2
+		#error "Singleton.hpp version error!"
+	#endif // LLANYLIB_SINGLETON_MAYOR_ || LLANYLIB_SINGLETON_MINOR_
+
+#else !defined(LLANYLIB_SINGLETON_HPP_)
+#define LLANYLIB_SINGLETON_HPP_
+#define LLANYLIB_SINGLETON_MAYOR_ 4
+#define LLANYLIB_SINGLETON_MINOR_ 2
+
+#include "definitions.hpp"
+
+namespace llcpp {
+namespace singleton {
+
+/*
+	private:
+		#pragma region Singleton
+		friend class Singleton<class>;
+		class();
+		~class();
+		#pragma endregion
+*/
+
+namespace static_ {
+
+template <class T>
+class Singleton {
+	protected:
+		Singleton() {}
+		~Singleton() {}
+	public:
+		static constexpr __LL_NODISCARD__ T& getInstance() __LL_EXCEPT__ {
+			static T instance;
+			return instance;
+		}
+};
+
+} // namespace static_
+
+namespace dynamic {
+
+template <class T>
+class Singleton {
+	protected:
+		Singleton() {}
+		~Singleton() {}
+		static T* instance;
+	public:
+		static __LL_NODISCARD__ T& getInstance() __LL_EXCEPT__ {
+			if (!Singleton<T>::instance)
+				Singleton<T>::instance = new T();
+			return *Singleton<T>::instance;
+		}
+		static void freeInstance() __LL_EXCEPT__ {
+			if (Singleton<T>::instance) {
+				delete Singleton<T>::instance;
+				Singleton<T>::instance = LL_NULLPTR;
+			}
+		}
+};
+
+#pragma region Singleton
+template <class T>
+T* Singleton<T>::instance = LL_NULLPTR;
+#pragma endregion
+
+} // namespace dynamic
+
+} // namespace singleton
+} // namespace llcpp
+
+#endif // !LLANYLIB_SINGLETON_HPP_

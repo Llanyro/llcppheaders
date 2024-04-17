4da3aa44c5f6ed224fbf65bf1ebb1cf8122610a9
Llanyro <Llanyro@gmail.com>
2024-04-17 18:34:56 +0200

    Update traits

--- llanylib/traits.hpp
+++ llanylib/traits.hpp
@@ -37,11 +37,15 @@ namespace traits {
 template<class T>
 __LL_VAR_INLINE__ constexpr ll_bool_t is_basic_type_v = std::_Is_any_of_v<std::remove_const_t<T>, ui8, ui16, ui32, ui64, i8, i16, i32, i64, f32, f64>;
 template<class T>
-__LL_VAR_INLINE__ constexpr ll_bool_t is_basic_type_or_ptr_v = is_basic_type_v<T> || std::is_pointer_v<T>;
+__LL_VAR_INLINE__ constexpr ll_bool_t is_basic_bigger_type_v = std::_Is_any_of_v<std::remove_const_t<T>, i128, i256, ui128, ui256>;
 template<class T>
 __LL_VAR_INLINE__ constexpr ll_bool_t is_floating_type_v = std::_Is_any_of_v<std::remove_const_t<T>, f32, f64>;
 template<class T>
+__LL_VAR_INLINE__ constexpr ll_bool_t is_floating_bigger_type_v = std::_Is_any_of_v<std::remove_const_t<T>, f128>;
+template<class T>
 __LL_VAR_INLINE__ constexpr ll_bool_t is_char_type_v = std::_Is_any_of_v<std::remove_const_t<T>, ll_char_t, ll_uchar_t, ll_wchar_t>;
+
+
 
 // Returns a type with reference if object is not basic type
 // Is only used when type parameter will be const

7ac29cdc23b2f3f7ff895d6ac2b79a8b602b8a7b
Llanyro <Llanyro@gmail.com>
2024-04-28 00:16:21 +0200

    Update node to new version

--- llanylib/Node.hpp
+++ llanylib/Node.hpp
@@ -25,40 +25,40 @@ namespace linked {
 template <class NodeType>
 class LL_SHARED_LIB NodeNext {
 	public:
-		__LL_CLASS_TEMPLATE_CUSTOM_TYPE__(NodeType, NodeType);
-		__LL_CLASS_TEMPLATE_CUSTOM_TYPE__(NodeNext<NodeType>, NodeNext);
+		using __NodeType = traits::template_types<NodeType>;
+		using __NodeNext = traits::template_types<NodeNext<NodeType>>;
 		//static_assert(std::is_base_of_v<__NodeNext, __NodeType>);
 	private:
-		__ptr_NodeType next;
+		__NodeType::ptr next;
 	public:
 		constexpr NodeNext() __LL_EXCEPT__ : NodeNext(LL_NULLPTR) {}
-		constexpr NodeNext(__ptr_NodeType next) __LL_EXCEPT__ : next(next) {}
+		constexpr NodeNext(__NodeType::ptr next) __LL_EXCEPT__ : next(next) {}
 		constexpr ~NodeNext() __LL_EXCEPT__ {}
 
-		NodeNext(__cref_NodeNext) __LL_EXCEPT__ = delete;
-		__ref_NodeNext operator=(__cref_NodeNext) __LL_EXCEPT__ = delete;
-		NodeNext(__move_NodeNext) __LL_EXCEPT__ = delete;
-		__ref_NodeNext operator=(__move_NodeNext) __LL_EXCEPT__ = delete;
+		NodeNext(__NodeNext::cref) __LL_EXCEPT__ = delete;
+		__NodeNext::ref operator=(__NodeNext::cref) __LL_EXCEPT__ = delete;
+		NodeNext(__NodeNext::move) __LL_EXCEPT__ = delete;
+		__NodeNext::ref operator=(__NodeNext::move) __LL_EXCEPT__ = delete;
 
-		__LL_NODISCARD__ constexpr __ptr_NodeType getNext() __LL_EXCEPT__ { return this->next; }
-		constexpr void setNext(__ptr_NodeType next) __LL_EXCEPT__ { this->next = next; }
-		__LL_NODISCARD__ constexpr __cptr_NodeType getNext() const __LL_EXCEPT__ { return this->next; }
+		__LL_NODISCARD__ constexpr __NodeType::ptr getNext() __LL_EXCEPT__ { return this->next; }
+		constexpr void setNext(__NodeType::ptr next) __LL_EXCEPT__ { this->next = next; }
+		__LL_NODISCARD__ constexpr __NodeType::cptr getNext() const __LL_EXCEPT__ { return this->next; }
 		constexpr void clear() __LL_EXCEPT__ {}
 
-		__LL_NODISCARD__ constexpr __ptr_NodeType getNextNoThis() __LL_EXCEPT__ {
+		__LL_NODISCARD__ constexpr __NodeType::ptr getNextNoThis() __LL_EXCEPT__ {
 			return (this->getNext() != this) ? this->getNext() : LL_NULLPTR;
 		}
-		__LL_NODISCARD__ constexpr __cptr_NodeType getNextNoThis() const __LL_EXCEPT__ {
+		__LL_NODISCARD__ constexpr __NodeType::cptr getNextNoThis() const __LL_EXCEPT__ {
 			return (this->getNext() != this) ? this->getNext() : LL_NULLPTR;
 		}
-		__LL_NODISCARD__ constexpr __ptr_NodeType getNext(const len_t &num) __LL_EXCEPT__ {
-			__ptr_NodeType result = this;
+		__LL_NODISCARD__ constexpr __NodeType::ptr getNext(const len_t &num) __LL_EXCEPT__ {
+			typename __NodeType::ptr result = this;
 			for (len_t i{}; i < num; ++i)
 				result = result->getNext();
 			return result;
 		}
-		__LL_NODISCARD__ constexpr __cptr_NodeType getNext(const len_t &num) const __LL_EXCEPT__ {
-			__cptr_NodeType result = this;
+		__LL_NODISCARD__ constexpr __NodeType::cptr getNext(const len_t &num) const __LL_EXCEPT__ {
+			typename __NodeType::cptr result = this;
 			for (len_t i{}; i < num; ++i)
 				result = result->getNext();
 			return result;
@@ -68,40 +68,40 @@ class LL_SHARED_LIB NodeNext {
 template <class NodeType>
 class LL_SHARED_LIB NodePrev {
 	public:
-		__LL_CLASS_TEMPLATE_CUSTOM_TYPE__(NodeType, NodeType);
-		__LL_CLASS_TEMPLATE_CUSTOM_TYPE__(NodePrev<NodeType>, NodePrev);
+		using __NodeType = traits::template_types<NodeType>;
+		using __NodePrev = traits::template_types<NodePrev<NodeType>>;
 		//static_assert(std::is_base_of_v<__NodePrev, __NodeType>);
 	private:
-		__ptr_NodeType prev;
+		__NodeType::ptr prev;
 	public:
 		constexpr NodePrev() __LL_EXCEPT__ : NodePrev(LL_NULLPTR) {}
-		constexpr NodePrev(__ptr_NodeType prev) __LL_EXCEPT__ : prev(prev) {}
+		constexpr NodePrev(__NodeType::ptr prev) __LL_EXCEPT__ : prev(prev) {}
 		constexpr ~NodePrev() __LL_EXCEPT__ {}
 
-		NodePrev(__cref_NodePrev) __LL_EXCEPT__ = delete;
-		__ref_NodePrev operator=(__cref_NodePrev) __LL_EXCEPT__ = delete;
-		NodePrev(__move_NodePrev) __LL_EXCEPT__ = delete;
-		__ref_NodePrev operator=(__move_NodePrev) __LL_EXCEPT__ = delete;
+		NodePrev(__NodePrev::cref) __LL_EXCEPT__ = delete;
+		__NodePrev::ref operator=(__NodePrev::cref) __LL_EXCEPT__ = delete;
+		NodePrev(__NodePrev::move) __LL_EXCEPT__ = delete;
+		__NodePrev::ref operator=(__NodePrev::move) __LL_EXCEPT__ = delete;
 
-		__LL_NODISCARD__ constexpr __ptr_NodeType getPrev() __LL_EXCEPT__ { return this->prev; }
-		constexpr void setPrev(__ptr_NodeType prev) __LL_EXCEPT__ { this->prev = prev; }
-		__LL_NODISCARD__ constexpr __cptr_NodeType getPrev() const __LL_EXCEPT__ { return this->prev; }
+		__LL_NODISCARD__ constexpr __NodeType::ptr getPrev() __LL_EXCEPT__ { return this->prev; }
+		constexpr void setPrev(__NodeType::ptr prev) __LL_EXCEPT__ { this->prev = prev; }
+		__LL_NODISCARD__ constexpr __NodeType::cptr getPrev() const __LL_EXCEPT__ { return this->prev; }
 		constexpr void clear() __LL_EXCEPT__ {}
 
-		__LL_NODISCARD__ constexpr __ptr_NodeType getPrevNoThis() __LL_EXCEPT__ {
+		__LL_NODISCARD__ constexpr __NodeType::ptr getPrevNoThis() __LL_EXCEPT__ {
 			return (this->getPrev() != this) ? this->getPrev() : LL_NULLPTR;
 		}
-		__LL_NODISCARD__ constexpr __cptr_NodeType getPrevNoThis() const __LL_EXCEPT__ {
+		__LL_NODISCARD__ constexpr __NodeType::cptr getPrevNoThis() const __LL_EXCEPT__ {
 			return (this->getPrev() != this) ? this->getPrev() : LL_NULLPTR;
 		}
-		__LL_NODISCARD__ constexpr __ptr_NodeType getPrev(const len_t num) __LL_EXCEPT__ {
-			__ptr_NodeType result = this;
+		__LL_NODISCARD__ constexpr __NodeType::ptr getPrev(const len_t num) __LL_EXCEPT__ {
+			__NodeType::ptr result = this;
 			for (len_t i{}; i < num; ++i)
 				result = result->getPrev();
 			return result;
 		}
-		__LL_NODISCARD__ constexpr __cptr_NodeType getPrev(const len_t num) const __LL_EXCEPT__ {
-			__cptr_NodeType result = this;
+		__LL_NODISCARD__ constexpr __NodeType::cptr getPrev(const len_t num) const __LL_EXCEPT__ {
+			__NodeType::cptr result = this;
 			for (len_t i{}; i < num; ++i)
 				result = result->getPrev();
 			return result;

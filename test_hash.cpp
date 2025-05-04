1ac6976d64cf79f1deee7fde06fc0514f315a575
Llanyro <Llanyro@gmail.com>
2025-05-04 17:15:17 +0200

    Update hash tests

--- test_hash.cpp
+++ test_hash.cpp
@@ -3,12 +3,12 @@
 #include "llanylib/hash/CityHash.hpp"
 #include "llanylib/hash/LlanyHash.hpp"
 
-constexpr auto hash1 = ::llcpp::meta::utils::hash::CityHash<>().cityHash64("asdf", 4);
-constexpr auto hash2 = ::llcpp::meta::utils::hash::CityHash<>().cityHash64("asdf", 4);
+constexpr auto hash1 = ::llcpp::meta::hash::CityHash<>().cityHash64("asdf", 4);
+constexpr auto hash2 = ::llcpp::meta::hash::CityHash<>().cityHash64("asdf", 4);
 
-constexpr auto hash3 = ::llcpp::meta::utils::hash::LlanyHash<>().llanyHash64_v1("asdf", 4);
-constexpr auto hash4 = ::llcpp::meta::utils::hash::LlanyHash<>().llanyHash64_v2("asdf", 4);
-constexpr auto hash5 = ::llcpp::meta::utils::hash::LlanyHash<>().llanyHash64_v3("asdf", 4);
+constexpr auto hash3 = ::llcpp::meta::hash::LlanyHash<>().llanyHash64_v1("asdf", 4);
+constexpr auto hash4 = ::llcpp::meta::hash::LlanyHash<>().llanyHash64_v2("asdf", 4);
+constexpr auto hash5 = ::llcpp::meta::hash::LlanyHash<>().llanyHash64_v3("asdf", 4);
 
 int main() {
 	::std::fwrite(__PRETTY_FUNCTION__, sizeof(__PRETTY_FUNCTION__), 1, stdout);
@@ -17,7 +17,7 @@ int main() {
 	::std::printf("\n%llu\n", hash3);
 	::std::printf("\n%llu\n", hash4);
 	::std::printf("\n%llu\n", hash5);
-	auto hash6 = ::llcpp::meta::utils::hash::LlanyHash<>().llanyHash64Combine("asdf", 4);
+	auto hash6 = ::llcpp::meta::hash::LlanyHash<>().llanyHash64Combine("asdf", 4);
 	::std::printf("\n%llu\n", hash6);
 	return 0;
 }
